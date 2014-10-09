6ffd2945ad649896c326f1be2e878e49cb06cad2
Christian Groll <groll.christian.edu@gmail.com>
2014-10-09 11:53:11 +0200

    loading of Dates changed

--- src/EconDatasets.jl
+++ src/EconDatasets.jl
@@ -1,13 +1,13 @@
 ## list packages that shall be automatically loaded
 using DataFrames
-using Dates
+using Base.Dates
 using TimeData
 
 module EconDatasets
 
 
 using DataFrames
-using Dates
+using Base.Dates
 using TimeData
 
 export dataset,
