81d814c11904489e4fd638d0de60052bdd19ce55
Christian Groll <groll.christian.edu@gmail.com>
2014-10-12 11:33:58 +0200

    added table headers for sp500 wikipedia tickers

--- src/getDataset/getSP500TickerSymbols.jl
+++ src/getDataset/getSP500TickerSymbols.jl
@@ -6,6 +6,15 @@ open |>
 readall |>
 parsehtml
 
+## tableHeaders = [kk.root[2][3][4][4][7][1][1][1][1][1].text,
+##                 kk.root[2][3][4][4][7][1][1][2][1].text,
+##                 kk.root[2][3][4][4][7][1][1][3][1][1].text,
+##                 kk.root[2][3][4][4][7][1][1][4][1][1].text,
+##                 kk.root[2][3][4][4][7][1][1][5][1].text,
+##                 kk.root[2][3][4][4][7][1][1][6][1].text,
+##                 kk.root[2][3][4][4][7][1][1][7][1].text,
+##                 kk.root[2][3][4][4][7][1][1][8][1][1].text]
+
 function accessTableEntry(htmlContent::HTMLDocument, ii::Int)
     return htmlContent.root[2][3][4][4][7][1][ii+1][1][1][1].text
 end
